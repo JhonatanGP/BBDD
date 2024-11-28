1211b7d81b79fd993912df816c1d5958eebd0402
JhonatanGP <jguzman5292@ceuandalucia.es>
2024-11-28 20:45:04 +0100

    G

--- 1TRIM/SQL/TEMA_5.sql
+++ 1TRIM/SQL/TEMA_5.sql
@@ -22,130 +22,138 @@ create table estudiantes(
     apellidos varchar(100),
     edad int
 );
-insert into estudiantes (id,nombre,apellidos) 
-    values (1,'Luis','García');
-insert into estudiantes (id,nombre) 
-    values (2,'Pedro');
+insert into estudiantes (id,nombre,apellidos) values (1,'Luis','García');
+insert into estudiantes (id,nombre) values (2,'Pedro');
 --select * from estudiantes;
  
 --Actividad 2
 insert into estudiantes (id,nombre,apellidos)
 	values (1,'Luis','García');--No puedo tener 2 ids valor 1
-insert into estudiantes 
-    values (3,'Antonio','Domínguez',21,'Sevilla');--Falta columna
-insert into estudiantes (id,nombre,apellidos)
-	values (11,'Luis','García');
-alter table estudiantes
-    add localidad varchar2(50);
-insert into estudiantes 
-    values (3,'Antonio','Domínguez',21,'Sevilla');
-insert into estudiantes
-    values (4,'Pedro','Pérez',22,null);
+insert into estudiantes values (3,'Antonio','Domínguez',21,'Sevilla');--Falta columna
+insert into estudiantes (id,nombre,apellidos) values (11,'Luis','García');
+alter table estudiantes add localidad varchar2(50);
+insert into estudiantes values (3,'Antonio','Domínguez',21,'Sevilla');
+insert into estudiantes values (4,'Pedro','Pérez',22,null);
 select * from estudiantes;
 --Actividad 3
-alter table estudiantes
-	add ciclo varchar2(50) default 'DAMDAW';
+alter table estudiantes	add ciclo varchar2(50) default 'DAMDAW';
 insert into estudiantes (id,nombre) values (5,'María');
 insert into estudiantes (id,nombre,ciclo) values (6,'Tomás','DAW');
+insert into estudiantes values (7,'Tomás',null,null,null,'DAW');
 select * from estudiantes;
 
--- PRACTICA EXAMEN 
-drop table estaciones cascade constraints;
-create table estaciones(
-    nombre varchar2(50) primary key,
-    num_andenes int,
-    volumen_entr number,
-    direccion varchar2(50),
-    tfno_info varchar2(50),
-    localidad varchar2(50),
-    num_empresas int,
-    dni_responsable char(9)
+update estudiantes set apellidos = 'González' where id = 1;
+update estudiantes set nombre = 'Pedro Luis', edad = 30 where id = 3;
+update estudiantes set ciclo = 'DAW';
+update estudiantes set nombre = 'Jua' where id = 2;
+update estudiantes set nombre = 'Juan' where id = 2;
+--'28/11/2024' '28-11-2024' '28/11/24'
+update estudiantes set edad = 23 where id = 2 or id = 3;
+update estudiantes set edad = 24 where id in (1,2,3);
+/*Actividad 4
+Se han preguntado las edades de Luis y Pedro (23 y 19 años respectivamente). 
+Modifica sus registros de la tabla estudiantes. */
+insert into estudiantes (id,nombre) values (8,'Luis'); 
+insert into estudiantes (id,nombre) values (9,'Pedro'); 
+update estudiantes set edad = 23 where id = 8;
+update estudiantes set edad = 19 where id = 9;
+/* Actividad 5
+Cambia todos los registros de la tabla estudiantes para que tengan 19 años de edad.*/
+update estudiantes set edad = 19;
+/*Actividad 6
+Borra el registro con id número 3 de la tabla estudiantes.*/
+delete from estudiantes where id = 3;
+--ejemplo
+drop table notas cascade constraints;
+create table notas(
+    id int primary key,
+    valor number(3,1),
+    id_estudiante int,
+    foreign key(id_estudiante) references estudiantes(id)
 );
-drop table responsables cascade constraints;
-create table responsables(
-    dni char(9) check(regexp_like(dni,'[0-9]{8}[A-Z]')) primary key,
-    nombre varchar2(50),
-    apellidos varchar2(50),
-    edad int,
-    direccion varchar2(50)
+insert into notas values (1,7,1);
+insert into notas values (2,4,2);
+select * from notas;
+delete from estudiantes where id = 2; -- no se puede, habría que hacer lo de abajo
+
+--ejemplo on delete cascade
+drop table notas cascade constraints;
+create table notas(
+    id int primary key,
+    valor number(3,1),
+    id_estudiante int,
+    foreign key(id_estudiante) references estudiantes(id) on delete cascade
 );
-drop table telefonos cascade constraints;
-create table telefonos(
-    numero int primary key,
-    tipo varchar2(50)
+insert into notas values (1,7,1);
+insert into notas values (2,4,2);
+select * from notas;
+delete from estudiantes where id = 2;
+select * from notas;
+
+--ejemplo on delete set null
+drop table notas cascade constraints;
+create table notas(
+    id int primary key,
+    valor number(3,1),
+    id_estudiante int,
+    foreign key(id_estudiante) references estudiantes(id) on delete set null
 );
-drop table trayectos cascade constraints;
-create table trayectos(
-    codigo int primary key,
-    num_plazas_v int,
-    hora_llegada varchar2(50),
-    fecha_llegada date,
-    hora_salida varchar2(50),
-    fecha_salida date,
-    direccion varchar2(50),
-    nombre_estacion_origen varchar2(50),
-    nombre_estacion_destino varchar2(50),
-    codigo_empresa int,
-    foreign key(nombre_estacion_origen) 
-        references estaciones(nombre),
-    constraint fk_nombre_estacion_destino_trayectos 
-        foreign key(nombre_estacion_destino) 
-        references estaciones(nombre)
+insert into notas values (1,7,1);
+insert into notas values (2,4,2);
+select * from notas;
+delete from estudiantes where id = 2;
+select * from notas;
+
+
+-- PDF EJERCICICIOS
+DROP TABLE BARCOS CASCADE CONSTRAINTS;
+CREATE TABLE BARCOS(
+    MATRICULA CHAR(7) PRIMARY KEY check (REGEXP_LIKE( MATRICULA, '[A-Z][A-Z]-[0-9][0-9][0-9][0-9]')),
+    NOMBRE VARCHAR(50),
+    CLASE VARCHAR(50),
+    ARMADOR VARCHAR(50),
+    CAPACIDAD NUMBER(8,2),
+    NACIONALIDAD VARCHAR(50)
 );
-drop table trabajadores cascade constraints;
-create table trabajadores(
-    dni char(9) primary key,
-    nombre varchar2(50),
-    direccion varchar2(50),
-    apellidos varchar2(50),
-    tipoTrabajador varchar2(50),
-    numero_carne_conductor int,
-    anos_carne_conductor int,
-    turno_venta varchar2(50),
-    codigo_empresa int
+
+DROP TABLE LOTES CASCADE CONSTRAINTS;
+CREATE TABLE LOTES(
+    CODIGO INT PRIMARY KEY,
+    MATRICULA CHAR(7),
+    NUM_KG NUMBER(8,2) CHECK (NUM_KG>0),
+    PRECIO_KG_SALIDA NUMBER(8,2) CHECK (PRECIO_KG_SALIDA>0),
+    PRECIO_KG_VENDIDO NUMBER(8,2) CHECK (PRECIO_KG_VENDIDO>0),
+    FECHA_VENTA DATE NOT NULL, --PARA PONER QUE LA FECHA NO ADMITE NULL
+    COD_ESPECIE INT
 );
-drop table autobuses cascade constraints;
-create table autobuses(
-    num_bastidor int primary key,
-    matricula varchar2(50),
-    anos int,
-    fecha_rev date,
-    kms int,
-    codigo_empresa int
+
+DROP TABLE ESPECIES CASCADE CONSTRAINTS;
+CREATE TABLE ESPECIES(
+    CODIGO INT PRIMARY KEY,
+    NOMBRE VARCHAR(50),
+    TIPO VARCHAR(50),
+    CUPO_BARCO NUMBER(8,2),
+    CALADERO_PPAL INT
 );
-drop table empresas cascade constraints;
-create table empresas(
-    codigo int primary key,
-    cif char(9),
-    nombre_comercial varchar2(50),
-    direccion varchar2(50),
-    codigo_empresa_filial int,
-    foreign key(codigo_empresa_filial) references empresas(codigo)
+
+DROP TABLE CALADEROS CASCADE CONSTRAINTS;
+CREATE TABLE CALADEROS(
+    CODIGO INT PRIMARY KEY,
+    NOMBRE VARCHAR(50) CHECK (NOMBRE LIKE UPPER(NOMBRE)),
+    UBICACION VARCHAR(100) CHECK (UBICACION LIKE UPPER(UBICACION)),
+    ESPECIE_PPAL INT
 );
-drop table estaciones_empresas cascade constraints;
-create table estaciones_empresas(
-    nombre_estacion varchar2(50),
-    codigo_empresa int,
-    primary key(nombre_estacion,codigo_empresa),
-    foreign key(nombre_estacion) references estaciones(nombre),
-    foreign key(codigo_empresa) references empresas(codigo)
+
+DROP TABLE FECHAS_CAPTURAS CASCADE CONSTRAINTS;
+CREATE TABLE FECHAS_CAPTURAS(
+    COD_ESPECIE INT,
+    COD_CALADERO INT,
+    FECHA_INICIO DATE CHECK ( FECHA_INICIO NOT BETWEEN DATE '2020-02-02' AND DATE '2020-03-28' ),
+    FECHA_FIN DATE CHECK ( FECHA_FIN NOT BETWEEN DATE '2020-02-02' AND DATE '2020-03-28' ),
+    PRIMARY KEY( COD_ESPECIE, COD_CALADERO )
 );
-alter table estaciones
-    add foreign key(dni_responsable) references responsables(dni);
-alter table trayectos
-    add foreign key(codigo_empresa) references empresas(codigo);
-alter table trabajadores
-    add foreign key(codigo_empresa) references empresas(codigo);
-alter table autobuses
-    add foreign key(codigo_empresa) references empresas(codigo);
---1. Crear un campo stock en estaciones_empresas que sea integer
-alter table estaciones_empresas add stock integer;
---2. Modifica el tipo de dato anterior para que sea un number con 4 cifras y una de ellas decimal
-alter table estaciones_empresas modify stock number(4,1);
---3. Elimina la PK de estaciones_empresas
- alter table estaciones_empresas drop primary key;
---4. Quiero que el campo nombre_estacion sea ahora PK y ponle el nombre a la restricción siguiente: pk_nombre_estacion_empresa
- alter table estaciones_empresas add constraint pk_nombre_estacion_empresa primary key(nombre_estacion);
---5. Cambia el nombre de la restricción anterior por el siguiente: pk_nom_est_emp
- 
---6. Haz lo que tengas que hacer (lo que sea necesario) para borrar la tabla estaciones_empresas sin usar cascade constraints.
+
+
+
+    
+
