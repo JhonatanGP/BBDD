8712c2a3182900d12c442ad23d2435299cb5b2f4
JhonatanGP <jguzman5292@ceuandalucia.es>
2025-02-05 20:39:07 +0100

    f

--- 2TRIM/EJERCICIOS_CLASE/TEMA 6.sql
+++ 2TRIM/EJERCICIOS_CLASE/TEMA 6.sql
@@ -488,6 +488,29 @@ begin
     dbms_output.put_line(num1/num2);
 end;
 /
+
+--EJERCICIO 10 con IF / ELSIF / ELSE
+/*Crea un programa que tome pida una variable al usuario para que la introduzca por teclado (tiene que ser un número), y según sea su valor 
+entre 0 y 10, devuelva por pantalla el valor de la nota: SUSPENSO, APROBADO, BIEN, NOTABLE,SOBRESALIENTE. En caso contrario, que devuelva
+‘El valor introducido es incorrecto’.*/
+declare
+    nota number(3,1) := &nota;
+begin
+    if nota < 5 and nota >= 0 then
+        dbms_output.put_line('SUSPENSO');
+    elsif nota >=5 and nota < 6 then
+        dbms_output.put_line('APROBADO');
+    elsif nota >=6 and nota < 7 then
+        dbms_output.put_line('BIEN');
+    elsif nota >=7 and nota < 9 then
+        dbms_output.put_line('NOTABLE');
+    elsif nota >=9 and nota <= 10 then
+        dbms_output.put_line('SOBRESALIENTE');
+    else
+        dbms_output.put_line('El valor introducido es incorrecto');
+    end if;
+end;
+/
 --EJERCICIO INDA
 /*PIDE AL USUARIO DETNO, DNAME Y LOC Y METE ESOS VALORES CON UN INSERT IN INTO EN LA TABLA DEPT. EJ: 50,MARKETING,SEVILLA*/
 DECLARE 
@@ -510,5 +533,169 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('FILA INSERTADA CORRECTAMENTE: ' || NUMDEPARTAMENTO || '-' || NOMBREDEPARTAMENTO || '-' || LOCALIZACIONDEPARTAMENTO);
 END;
 /
+--JUEVES 05 DE FEBRERO DE 2025
+set serveroutput on;
+--EJEMPLO 1
+declare
+    numero int := &introduzca_numero;
+begin
+    case numero
+    when 1 then
+        dbms_output.put_line('Ha metido el 1');
+    when 2 then
+        dbms_output.put_line('Ha metido el 2');
+    else
+        dbms_output.put_line('Ha metido un número distinto de 1 y 2');
+    end case;
+end;
+/
+--EJEMPLO 2
 
+declare
+    numero int := &introduzca_numero;
+begin
+    case 
+    when numero = 1 then
+        dbms_output.put_line('Ha metido el 1');
+    when numero = 2 or numero between 3 and 4 then
+        dbms_output.put_line('Ha metido el 2,3,4');
+    else
+        dbms_output.put_line('Ha metido un número distinto de 1 y 2');
+    end case;
+end;
+/
+--Realiza el mismo ejercicio 10 pero utilizando CASE en lugar de IF | ELSIF | END IF.
 
+DECLARE
+    NUM INT := &INTRODUZCA_NOTA;
+BEGIN
+    CASE 
+    when NUM between 0 and 4 then
+        dbms_output.put_line('SUSPENSO');
+    when NUM = 5 THEN
+        dbms_output.put_line('APROBADO');
+    when NUM = 6 THEN
+        dbms_output.put_line('BIEN');
+    when NUM = 7 or NUM = 8 then
+        dbms_output.put_line('NOTABLE');
+    when NUM = 9 or NUM = 10 then -- NUM >=9 AND NUM <=10 THEN
+        dbms_output.put_line('SOBRESALIENTE');
+    else
+        dbms_output.put_line('Ha metido un número distinto de 0 Y 10');
+    end case;
+END;
+/
+/*Ejercicio 12 SENCILLO.
+Realiza un programa que lea por teclado dos números enteros. El primero será los goles del equipo de casa, y el segundo los goles del equipo
+de fuera. Se quiere devolver por pantalla quién ha ganado: “El equipo de casa/visitante ha ganado”. En caso de empate se indicará 
+“El resultado del partido ha sido de empate”. */
+
+declare
+    eq1 INT := &goles_casa;
+    eq2 INT := &goles_fuera;
+begin
+     if eq1 > eq2  then
+        dbms_output.put_line('El equipo de casa ha ganado');
+    elsif eq1 = eq2 then
+        dbms_output.put_line('El resultado del partido ha sido de empate');
+    else
+        dbms_output.put_line('El equipo de fuera ha ganado');
+    end if;
+end;
+/
+
+ALTER SESSION SET nls_date_format='DD-MM-YYYY HH24:MI:SS';
+DROP TABLE FUTBOLISTAS CASCADE CONSTRAINTS;
+DROP TABLE EQUIPOS CASCADE CONSTRAINTS;
+DROP TABLE PARTIDOS CASCADE CONSTRAINTS;
+
+CREATE TABLE FUTBOLISTAS(
+ID CHAR(6) PRIMARY KEY CHECK( REGEXP_LIKE( ID, 'F[0-9][0-9][0-9]20' )),
+NOMBRE VARCHAR(100),
+APELLIDOS VARCHAR(300),
+FECHA_NACIMIENTO DATE,
+POSICION VARCHAR(50) CHECK( POSICION LIKE 'PORTERO' OR POSICION LIKE 'DEFENSA' OR POSICION LIKE 'MEDIOCENTRO' OR POSICION LIKE 'DELANTERO' ),
+SALARIO NUMBER(12,2) CHECK( SALARIO > 50000),
+ID_EQUIPO INT
+);
+
+CREATE TABLE EQUIPOS(
+ID INT PRIMARY KEY,
+NOMBRE VARCHAR(100),
+ESTADIO VARCHAR(100),
+PRESUPUESTO NUMBER(20,2)
+);
+
+CREATE TABLE PARTIDOS(
+ID INT PRIMARY KEY,
+ARBITRO VARCHAR(200),
+RESULTADO VARCHAR(5) CHECK( REGEXP_LIKE( RESULTADO, '[0-9][0-9]-[0-9][0-9]' )),
+ESTADIO VARCHAR(100),
+FECHA DATE,
+ID_EQUIPO_CASA INT,
+ID_EQUIPO_FUERA INT
+);
+
+ALTER TABLE FUTBOLISTAS ADD CHECK (FECHA_NACIMIENTO > '31/DEC/1974');
+ALTER TABLE FUTBOLISTAS ADD FOREIGN KEY (ID_EQUIPO) REFERENCES EQUIPOS(ID);
+ALTER TABLE PARTIDOS ADD FOREIGN KEY (ID_EQUIPO_CASA) REFERENCES EQUIPOS(ID);
+ALTER TABLE PARTIDOS ADD FOREIGN KEY (ID_EQUIPO_FUERA) REFERENCES EQUIPOS(ID);
+
+DELETE FROM EQUIPOS;
+INSERT INTO EQUIPOS VALUES (1,'EQUIPO A', 'ESTADIO A',1000000);
+INSERT INTO EQUIPOS VALUES (2,'EQUIPO B', 'ESTADIO B',2000000);
+INSERT INTO EQUIPOS VALUES (3,'EQUIPO C', 'ESTADIO C',3000000);
+
+DELETE FROM FUTBOLISTAS;
+INSERT INTO FUTBOLISTAS VALUES ('F00120','PEDRO','GOMEZ','22/01/1980','PORTERO',100000,1);
+INSERT INTO FUTBOLISTAS VALUES ('F00220','PEDRO LUIS','GOMEZ','20/04/1985','DEFENSA',110000,1);
+INSERT INTO FUTBOLISTAS VALUES ('F00320','LUIS','GALVEZ','12/01/1990','DELANTERO',80000,1);
+INSERT INTO FUTBOLISTAS VALUES ('F00420','ANTONIO','DOMINGUEZ','25/06/1981','PORTERO',340000,2);
+INSERT INTO FUTBOLISTAS VALUES ('F00520','JESUS','FERNANDEZ','02/01/1995','MEDIOCENTRO',140000,2);
+INSERT INTO FUTBOLISTAS VALUES ('F00620','DIEGO','DIAZ','05/04/1987','PORTERO',160000,2);
+INSERT INTO FUTBOLISTAS VALUES ('F00720','PABLO','CARO','27/01/1988','DEFENSA',200000,3);
+INSERT INTO FUTBOLISTAS VALUES ('F00820','ESTEBAN','BENITEZ','29/09/2000','DEFENSA',300000,3);
+INSERT INTO FUTBOLISTAS VALUES ('F00920','ENRIQUE','ALVAREZ','30/09/2001','MEDIOCENTRO',120000,3);
+
+DELETE FROM PARTIDOS;
+INSERT INTO PARTIDOS VALUES (1,'ARBITRO A','00-00','ESTADIO A','01/01/2020',1,2);
+INSERT INTO PARTIDOS VALUES (2,'ARBITRO B','01-00','ESTADIO A','08/01/2020',1,3);
+INSERT INTO PARTIDOS VALUES (3,'ARBITRO C','01-01','ESTADIO B','15/01/2020',2,3);
+INSERT INTO PARTIDOS VALUES (4,'ARBITRO B','02-00','ESTADIO B','22/01/2020',2,1);
+INSERT INTO PARTIDOS VALUES (5,'ARBITRO A','04-01','ESTADIO C','29/01/2020',3,1);
+INSERT INTO PARTIDOS VALUES (6,'ARBITRO A','03-03','ESTADIO C','05/02/2020',3,2);
+
+UPDATE FUTBOLISTAS SET ID_EQUIPO = 2 WHERE ID LIKE 'F00120';
+UPDATE FUTBOLISTAS SET ID_EQUIPO = 3 WHERE ID LIKE 'F00420';
+UPDATE FUTBOLISTAS SET ID_EQUIPO = 1 WHERE ID LIKE 'F00920';
+
+DELETE FROM FUTBOLISTAS WHERE ID LIKE 'F00120' OR ID LIKE 'F00420';
+
+ALTER TABLE FUTBOLISTAS ADD ALTURA INT;
+ALTER TABLE FUTBOLISTAS ADD PESO NUMBER(4,1);
+UPDATE FUTBOLISTAS SET ALTURA = 179, PESO = 76 WHERE NOMBRE LIKE 'PEDRO LUIS';
+UPDATE FUTBOLISTAS SET ALTURA = 174, PESO = 71.5 WHERE NOMBRE LIKE 'LUIS';
+UPDATE FUTBOLISTAS SET ALTURA = 169, PESO = 66 WHERE NOMBRE LIKE 'JESUS';
+UPDATE FUTBOLISTAS SET ALTURA = 189, PESO = 82 WHERE NOMBRE LIKE 'DIEGO';
+UPDATE FUTBOLISTAS SET ALTURA = 183, PESO = 84 WHERE NOMBRE LIKE 'PABLO';
+UPDATE FUTBOLISTAS SET ALTURA = 180, PESO = 81.5 WHERE NOMBRE LIKE 'ESTEBAN';
+UPDATE FUTBOLISTAS SET ALTURA = 162, PESO = 60 WHERE NOMBRE LIKE 'ENRIQUE';
+
+--Ejercicio 12 | AMPLIACIÓN
+--A partir del campo RESULTADO de la tabla PARTIDOS para el día/fecha 08/01/2020, indica quién ha ganado
+declare
+    goles_casa int;
+    goles_fuera int;
+begin
+    select to_number(substr(resultado,1,2)) into goles_casa from partidos where fecha = '08/01/2020';
+    select to_number(substr(resultado,4,2)) into goles_fuera from partidos where fecha = '08/01/2020';
+    case
+    when goles_casa > goles_fuera then
+        dbms_output.put_line('El equipo de casa ha ganado');
+    when goles_casa < goles_fuera then
+        dbms_output.put_line('El equipo visitante ha ganado');
+    else
+        dbms_output.put_line('El resultado del partido ha sido empate');
+    end case;
+end;
+/
