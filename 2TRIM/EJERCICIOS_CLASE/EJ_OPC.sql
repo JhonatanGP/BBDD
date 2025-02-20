f9571ae4c8c40882335854e353ab9a43498abc94
JhonatanGP <jguzman5292@ceuandalucia.es>
2025-02-20 20:50:38 +0100

    c

--- 2TRIM/EJERCICIOS_CLASE/EJ_OPC.sql
+++ 2TRIM/EJERCICIOS_CLASE/EJ_OPC.sql
@@ -12,7 +12,6 @@ STARTUP RESTRICT;
 DROP DATABASE;
 */
 
-
 CREATE TABLE oficina (
   codigo_oficina VARCHAR(10) NOT NULL,
   ciudad VARCHAR(30) NOT NULL,
@@ -1029,3 +1028,256 @@ select distinct nombre,gama
 /* 4. Se quiere saber el código de los pedidos que no tienen comentarios y además la fecha del pedido se encuentra entre enero de 
 2008 y febrero de 2009. */
 select codigo_pedido from pedido where comentarios is null and fecha_pedido between to_date('2008-01-01') and to_date('2009-02-01');
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+drop table platos cascade constraints;
+drop table ingredientes cascade constraints;
+drop table ingredientes_platos cascade constraints;
+drop table clientes cascade constraints;
+drop table comandas cascade constraints;
+create table platos (
+    id int primary key,
+    nombre varchar(100) not null,
+    precio number(6,2),
+    tiempo_preparacion int
+);
+create table ingredientes(
+    id int primary key,
+    nombre varchar(100) not null,
+    categoria varchar(30) check(categoria in ('CARNE','PESCADO','CONDIMENTO','FRUTA','VERDURA','LACTEO','BEBIDA','LEGUMBRE','OTRO')),
+    estado varchar(30) check( estado in ('SOLIDO','LIQUIDO'))
+);
+create table ingredientes_platos(
+    id_ingrediente int,
+    id_plato int,
+    cantidad int
+);
+alter table ingredientes_platos
+add foreign key (id_ingrediente) references ingredientes(id);
+alter table ingredientes_platos
+add foreign key (id_plato) references platos(id);
+create table clientes(
+    id int primary key,
+    nombre varchar(100) not null,
+    telefono varchar(10),
+    localidad varchar(100) not null
+);
+create table comandas(
+    id int primary key,
+    mesa int,
+    id_cliente int,
+    id_plato int,
+    estado varchar(30) check( estado in ('COCINA','SERVIDO','COBRADO','DEVUELTO')),
+    fecha date,
+    hora char(5)
+);
+alter table comandas
+add foreign key (id_cliente) references clientes(id);
+alter table comandas
+add foreign key (id_plato) references platos(id);
+
+insert into platos values (1,'Plato Arroz caldoso',12.50,25);
+insert into platos values (2,'Plato Calamares con patatas',7.50,15);
+insert into platos values (3,'Entrante bacalao en aceite',4.50,10);
+insert into platos values (4,'Entrante Crema de zanahorias',3,10);
+insert into platos values (5,'Tarta de queso',5.50,10);
+
+insert into ingredientes values (1,'Ajo','VERDURA','SOLIDO');
+insert into ingredientes values (2,'Aceite de oliva virgen extra','CONDIMENTO','LIQUIDO');
+insert into ingredientes values (3,'Cebolla','VERDURA','SOLIDO');
+insert into ingredientes values (4,'Calamar','PESCADO','SOLIDO');
+insert into ingredientes values (5,'Patata','VERDURA','SOLIDO');
+insert into ingredientes values (6,'Pimiento','VERDURA','SOLIDO');
+insert into ingredientes values (7,'Vino blanco','BEBIDA','LIQUIDO');
+insert into ingredientes values (8,'Sal','CONDIMENTO','SOLIDO');
+insert into ingredientes values (9,'Pimienta','CONDIMENTO','SOLIDO');
+insert into ingredientes values (10,'Costilla de cerdo','CARNE','SOLIDO');
+insert into ingredientes values (11,'Guisante','VERDURA','SOLIDO');
+insert into ingredientes values (12,'Tomate','VERDURA','SOLIDO');
+insert into ingredientes values (13,'Pimenton dulce','CONDIMENTO','SOLIDO');
+insert into ingredientes values (14,'Agua','BEBIDA','LIQUIDO');
+insert into ingredientes values (15,'Caldo de ave','BEBIDA','LIQUIDO');
+insert into ingredientes values (16,'Arroz','LEGUMBRE','SOLIDO');
+insert into ingredientes values (17,'Bacalao','PESCADO','SOLIDO');
+insert into ingredientes values (18,'Guindilla','CONDIMENTO','SOLIDO');
+insert into ingredientes values (19,'Perejil','CONDIMENTO','SOLIDO');
+insert into ingredientes values (20,'Zanahoria','VERDURA','SOLIDO');
+insert into ingredientes values (21,'Caldo de verduras','CONDIMENTO','LIQUIDO');
+insert into ingredientes values (22,'Nata liquida','LACTEO','LIQUIDO');
+insert into ingredientes values (23,'Queso mascarpone','LACTEO','SOLIDO');
+insert into ingredientes values (24,'Leche','LACTEO','SOLIDO');
+insert into ingredientes values (25,'Esencia vainilla','CONDIMENTO','LIQUIDO');
+insert into ingredientes values (26,'Huevo','OTRO','SOLIDO');
+insert into ingredientes values (27,'Harina','OTRO','SOLIDO');
+insert into ingredientes values (28,'Frutos rojos','FRUTA','SOLIDO');
+
+insert into ingredientes_platos values (1,1,3);
+insert into ingredientes_platos values (2,1,30);
+insert into ingredientes_platos values (10,1,300);
+insert into ingredientes_platos values (11,1,60);
+insert into ingredientes_platos values (12,1,40);
+insert into ingredientes_platos values (13,1,5);
+insert into ingredientes_platos values (14,1,250);
+insert into ingredientes_platos values (15,1,400);
+insert into ingredientes_platos values (6,1,80);
+insert into ingredientes_platos values (8,1,10);
+insert into ingredientes_platos values (16,1,150);
+insert into ingredientes_platos values (4,2,400);
+insert into ingredientes_platos values (2,2,40);
+insert into ingredientes_platos values (3,2,100);
+insert into ingredientes_platos values (1,2,10);
+insert into ingredientes_platos values (5,2,120);
+insert into ingredientes_platos values (6,2,80);
+insert into ingredientes_platos values (7,2,120);
+insert into ingredientes_platos values (8,2,12);
+insert into ingredientes_platos values (9,2,8);
+insert into ingredientes_platos values (17,3,125);
+insert into ingredientes_platos values (1,3,5);
+insert into ingredientes_platos values (18,3,2);
+insert into ingredientes_platos values (2,3,20);
+insert into ingredientes_platos values (19,3,2);
+insert into ingredientes_platos values (3,4,25);
+insert into ingredientes_platos values (20,4,60);
+insert into ingredientes_platos values (21,4,250);
+insert into ingredientes_platos values (22,4,50);
+insert into ingredientes_platos values (8,4,5);
+insert into ingredientes_platos values (9,4,5);
+insert into ingredientes_platos values (2,4,15);
+insert into ingredientes_platos values (2,5,3);
+insert into ingredientes_platos values (23,5,60);
+insert into ingredientes_platos values (24,5,20);
+insert into ingredientes_platos values (25,5,1);
+insert into ingredientes_platos values (26,5,10);
+insert into ingredientes_platos values (27,5,10);
+insert into ingredientes_platos values (28,5,20);
+
+insert into clientes values (1,'Pedro Juan','111222333','Bormujos');
+insert into clientes values (2,'Jose Miguel','222333444','Bormujos');
+insert into clientes values (3,'Maria','333444555','Gines');
+
+insert into comandas values (1,1,1,3,'COCINA','01-03-2021','21:00');
+insert into comandas values (2,1,1,1,'COCINA','01-03-2021','21:00');
+insert into comandas values (3,2,2,3,'COCINA','01-03-2021','21:05');
+insert into comandas values (4,2,2,4,'COCINA','01-03-2021','21:05');
+insert into comandas values (5,1,1,3,'SERVIDO','01-03-2021','21:10');
+insert into comandas values (6,2,2,3,'SERVIDO','01-03-2021','21:15');
+insert into comandas values (7,2,2,4,'SERVIDO','01-03-2021','21:16');
+insert into comandas values (8,2,2,2,'COCINA','01-03-2021','21:16');
+insert into comandas values (9,1,1,1,'SERVIDO','01-03-2021','21:25');
+insert into comandas values (10,1,1,5,'COCINA','01-03-2021','21:25');
+insert into comandas values (11,2,2,5,'COCINA','01-03-2021','21:26');
+insert into comandas values (12,2,2,2,'SERVIDO','01-03-2021','21:35');
+insert into comandas values (13,1,1,5,'SERVIDO','01-03-2021','21:37');
+insert into comandas values (14,2,2,5,'SERVIDO','01-03-2021','21:38');
+insert into comandas values (15,1,1,3,'COBRADO','01-03-2021','21:50');
+insert into comandas values (16,1,1,1,'COBRADO','01-03-2021','21:50');
+insert into comandas values (17,1,1,5,'COBRADO','01-03-2021','21:50');
+insert into comandas values (18,1,2,3,'COBRADO','01-03-2021','21:50');
+insert into comandas values (19,1,2,4,'COBRADO','01-03-2021','21:50');
+insert into comandas values (20,1,2,2,'COBRADO','01-03-2021','21:50');
+insert into comandas values (21,1,2,5,'COBRADO','01-03-2021','21:50');
+insert into comandas values (22,1,3,3,'COCINA','02-03-2021','21:05');
+insert into comandas values (23,1,3,1,'COCINA','02-03-2021','21:10');
+insert into comandas values (24,1,3,2,'COCINA','02-03-2021','21:12');
+insert into comandas values (25,1,3,1,'DEVUELTO','02-03-2021','21:12');
+insert into comandas values (26,1,3,3,'SERVIDO','02-03-2021','21:15');
+insert into comandas values (27,1,3,2,'SERVIDO','02-03-2021','21:27');
+insert into comandas values (28,1,3,5,'COCINA','02-03-2021','21:27');
+insert into comandas values (29,1,3,5,'SERVIDO','02-03-2021','21:37');
+insert into comandas values (30,1,3,3,'COBRADO','02-03-2021','21:55');
+insert into comandas values (31,1,3,2,'COBRADO','02-03-2021','21:55');
+insert into comandas values (32,1,3,5,'COBRADO','02-03-2021','21:55');
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- JUEVES 20 DE FEBRERO DE 2025 
+ 
+/*1. Inserta el siguiente registro en la tabla platos:
+- id: 6
+- nombre: Solomillo a la carbonara
+- precio: 6.50
+- cantidad: 12 */
+INSERT INTO PLATOS VALUES (6,'Solomillo a la carbonara',6.50,12);
+
+/* 2. Actualiza el registro anterior de la tabla platos con los siguientes datos:
+ - nuevo valor de precio: 6.75 */
+update platos set precio = 6.75 where id = 6;
+
+-- 3. Borra el registro que creaste en el apartado 1.
+delete from platos where id = 6;
+select * from platos;
+
+/* 4. Crea un nuevo registro en la tabla clientes con los siguientes datos:
+- id: 4
+- nombre: 'Luis Miguel'
+- localidad: 'Gines' */
+insert into clientes values (4,'Luis Miguel',null,'Gines');
+ 
+/* 5. Modifica el registro anterior (apartado 4) incluyendo el siguiente valor: 
+- telefono: 288333444 */
+update clientes set telefono = '288333444' where id = 4;
+
+/*6. Se quiere modificar el nombre de un plato(tabla platos). Cambiar el nombre del plato cuya longitud de caracteres es la
+mayor de toda la tabla, añadiendo al final del nombre '- cocina de autor'. Ej.: Entrante Crema de zanahorias - cocina de autor.*/
+select * from platos;
+update platos set nombre = nombre || '- cocina de autor' where length(nombre) = (select max(length(nombre)) from platos);
+
+/*7. Se detecta una errata en la hora de la última comanda guardada el día 02-03-2021 a las 21:55. En realidad fue 
+a las 22:05, cambia dicho valor de hora solo en ese registro.*/
+select * from comandas;
+update comandas set hora = '22:05' where fecha = '02/03/2021' and hora = '21:55';
+
+--Supuesto que queremos hacerlo solo de un registro, el último de la misma hora y día
+select max(id) from comandas where fecha = '02/03/2021' and hora = '22:05';
+update comandas 
+    set hora = '22:05' 
+    where id = (select max(id) from comandas where fecha = '02/03/2021' and hora = '22:05');
+ 
+/*8. Indica qué modificarías de la tabla comandas si se quiere que, al borrar un cliente (registro de la tabla clientes), 
+se ponga a null el campo id_cliente de todos los registros de la tabla comandas que tengan dicho id de cliente. 
+No hace falta que lo hagas sobre tu sql, solo indica cómo sería tu alter table comandas.*/
+alter table comandas drop constraint SYS_C008668;
+alter table comandas add foreign key (id_cliente) references clientes(id) on delete set null;
+ 
+/*9. Se quiere una lista con el nombre de todos los ingredientes que sean PESCADO. Debes devolver el nombre con todas 
+las letras en minúsculas.*/
+select lower(nombre) from ingredientes where categoria = 'PESCADO'; 
+select initcap(nombre) from ingredientes where categoria = 'PESCADO'; 
+
+/*10. Se quiere saber todos los estados de comandas que haya, sin repetir el valor en el resultado y ordenados en orden 
+inverso al alfabético (z-a).*/
+select distinct estado from comandas order by 1 desc;
+select distinct estado,mesa from comandas order by estado desc, 2 asc;
+select estado from comandas group by estado;
+
+/*11. Se quiere saber cuántas comandas se realizaron entre las 21:10 y las 21:50 horas (ambas inclusive) el día 
+01/03/21 por el id_cliente = 2.*/
+select count (*) from comandas where hora between '21:10' and '21:50' and fecha = '01/03/2021' and id_cliente = 2;
+
+/*12. Realiza la misma SQL del apartado 11 pero en lugar de usar id_cliente = 2, se quiere usar que el nombre del 
+cliente sea Pedro Juan.*/
+select * from clientes;
+select * from comandas;
+select count (*) from comandas where hora between '21:10' and '21:50' and fecha = '01/03/2021' and id_cliente = 2;
+-- nombre = 'Pedro Juan'
+--join
+select count(*) 
+    from comandas 
+    join clientes on id_cliente = clientes.id
+    where hora between '21:10' and '21:50' and fecha = '01/03/2021' and nombre = 'Pedro Juan';
+--subconsulta
+select count(*) from comandas where hora between '21:10' and '21:50' and fecha = '01/03/2021' and 
+    id_cliente = (select id from clientes where nombre = 'Pedro Juan');
+ 
+/*13. Muestra el nombre y la localidad de los clientes que vivan en Gines. Debes usar alias para nombrar las columnas, 
+la primera será "Cli" y la segunda "LOC". También usa alias de tabla, llamándola datoscompletos, y úsalo para seleccionar 
+las columnas nombre y localidad.*/
+select datoscompletos.nombre "CLI", datoscompletos.localidad "LOC" from clientes datoscompletos where localidad = 'Gines';
+ 
+/*14. Muestra todos los datos de la tabla ingredientes en mayúsculas y que se encuentren en estado SOLIDO.*/
+select id ,upper(nombre),upper(categoria),upper(estado) from ingredientes where estado = 'SOLIDO';
+/*15. ¿Cuál es el cliente cuyo nombre tiene menos caracteres? Pon en una única columna el nombre del cliente: número 
+de caracteres. Ej.: Maria: 5*/
+select nombre || ': ' || length(nombre) from clientes where length(nombre) = (select min(length(nombre)) from clientes);
